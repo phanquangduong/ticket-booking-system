66d11066727811acdb058306db5924cc2da407be
DuongPQ <duongpq@hblab.vn>
2026-06-09 16:51:13 +0700

    Setup Prometheus service for API monitoring

--- start/src/main/java/com/StartApplication.java
+++ start/src/main/java/com/StartApplication.java
@@ -1,15 +1,16 @@
-package com.start;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication()
 //@EnableScheduling
 public class StartApplication {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
         SpringApplication.run(StartApplication.class, args);
     }
 
